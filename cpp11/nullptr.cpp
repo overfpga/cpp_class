3b9bfae10cb8f5ad911e38d3291b2bc458faaa89
chengzhuang <897174113@qq.com>
2023-11-06 16:21:51 +0800

    new file:   .vscode/settings.json 	new file:   cpp11/nullptr 	modified:   cpp11/nullptr.cpp

---
+++ cpp11/nullptr.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+// int main(){
+
+//     int a;
+//     std::cout<<a<<std::endl;
+//     a = NULL;
+//     std::cout<<a<<std::endl;
+
+
+//     return 0;
+// }
+
+int main(){
+
+    void*a;
+    int*b = (int*)a;
+
+
+    return 0;
+}
