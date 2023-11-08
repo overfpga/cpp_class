61942113b438c53a70233b1908824723da99e0f8
chengzhuang <897174113@qq.com>
2023-11-08 14:29:52 +0800

    modified:   cpp11/nullper/README.md 	modified:   cpp11/nullper/nullptr 	modified:   cpp11/nullper/nullptr.cpp

--- cpp11/nullper/nullptr.cpp
+++ cpp11/nullper/nullptr.cpp
@@ -13,11 +13,41 @@ using namespace std;
 //     return 0;
 // }
 
+
+/*
+
+
+void test(int num){
+    std::cout<<"helo world"<<num<<std::endl;
+    return ;
+}
+
+void test(void* v){
+    std::cout<<"helo world *"<<std::endl;
+    return ;
+}
+
+int main(){
+    //c++中NULL存在二义性，他既有整数0属性，又能表示指针，所以在参数类型是int或void类型都可以被NULL赋值
+    //nullptr在c++只表示空指针
+    test(nullptr);
+    
+    //void*类型不能进行隐士类型转换，可以进行显示类型转换
+    void*a;
+    int*b=a;
+
+    return 0;
+}
+
+*/
+
+
 int main(){
 
-    void*a;
-    int*b = (int*)a;
+    int valuse[] {1,2,3};
 
-
+    int a {};
+    int* b{};
+    
     return 0;
 }
